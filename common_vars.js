40ab806a8eabe83c7043266294fba8956a7d7042
Davide Bettio <davide.bettio@ispirata.com>
2021-07-05 17:42:37 +0200

    1.0 is stable, 1.1 is unreleased
    Signed-off-by: Davide Bettio <davide.bettio@ispirata.com>

--- common_vars.js
+++ common_vars.js
@@ -1,10 +1,14 @@
 var versionNodes = [
     {
-        version: "v1.0 (unreleased)", // TODO: Remove unreleased when released
+        version: "v1.1 (unreleased)", // TODO: Remove unreleased when released
+        url: "https://docs.astarte-platform.org/snapshot"
+    },
+    {
+        version: "v1.0 (latest stable)",
         url: "https://docs.astarte-platform.org/1.0"
     },
     {
-        version: "v0.11 (latest stable)",
+        version: "v0.11 (deprecated)",
         url: "https://docs.astarte-platform.org/0.11"
     },
     {
