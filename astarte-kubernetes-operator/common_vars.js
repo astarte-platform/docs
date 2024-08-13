caec08658535328d97f367dd20b6786ba899809f
Mattia Mazzucato <mattia.mazzucato@secomind.com>
2024-08-13 15:12:49 +0200

    Astarte Operator v24.5 is now the latest
    Signed-off-by: Mattia Mazzucato <mattia.mazzucato@secomind.com>

--- astarte-kubernetes-operator/common_vars.js
+++ astarte-kubernetes-operator/common_vars.js
@@ -1,10 +1,14 @@
 var versionNodes = [
     {
-        version: "v23.11 (unreleased)", // TODO: Remove unreleased when released
+        version: "snapshot (unreleased)", // TODO: Remove unreleased when released
         url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/snapshot"
     },
     {
-        version: "v23.5 (latest stable)",
+        version: "v24.5 (latest stable)",
+        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/24.5"
+    },
+    {
+        version: "v23.5 (stable)",
         url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/23.5"
     },
     {
