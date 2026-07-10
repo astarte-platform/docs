bdcd979c0a8e8ba391e86c6d9e4323e0e157e7ee
Luca Marchiori <luca.marchiori@secomind.com>
2026-07-10 09:21:29 +0200

    chore: bring Astarte Operator version list up to date
    Signed-off-by: Luca Marchiori <luca.marchiori@secomind.com>

--- astarte-kubernetes-operator/common_vars.js
+++ astarte-kubernetes-operator/common_vars.js
@@ -1,22 +1,26 @@
 var versionNodes = [
-    {
-        version: "snapshot (unreleased)", // TODO: Remove unreleased when released
-        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/snapshot"
-    },
-    {
-        version: "v26.5 (unreleased)",
-        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/26.5"
-    },
-    {
-        version: "v24.5 (latest stable)",
-        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/24.5"
-    },
-    {
-        version: "v23.5 (stable)",
-        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/23.5"
-    },
-    {
-        version: "v22.11 (stable)",
-        url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/22.11"
-    }
-]
+  {
+    version: "snapshot (unreleased)", // TODO: Remove unreleased when released
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/snapshot",
+  },
+  {
+    version: "v26.7 (unreleased)",
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/26.7",
+  },
+  {
+    version: "v26.5 (stable)",
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/26.5",
+  },
+  {
+    version: "v24.5 (stable)",
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/24.5",
+  },
+  {
+    version: "v23.5 (stable)",
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/23.5",
+  },
+  {
+    version: "v22.11 (stable)",
+    url: "https://docs.astarte-platform.org/astarte-kubernetes-operator/22.11",
+  },
+];
